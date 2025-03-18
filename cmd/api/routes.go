6cefda6b374196656a3ec7d53434dcb167ce0ac2
slava <kishman201317@gmail.com>
2025-03-18 19:11:17 +0400

    add comment

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -7,9 +7,13 @@ import (
 
 func (app *application) routes() http.Handler {
 	router := httprouter.New()
+	// custom handler
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
 
+	// healthcheck handler
 	router.HandlerFunc(http.MethodGet, "/api/v1/healthcheck", app.healthcheckHandler)
 
+	// movie handler
 	router.HandlerFunc(http.MethodPost, "/api/v1/movies", app.createMovieHandler)
 	router.HandlerFunc(http.MethodGet, "/api/v1/movies/:id", app.showMovieHandler)
 
