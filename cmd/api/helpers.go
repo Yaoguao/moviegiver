31fd958c0a1048250c08f022648ae58db32f734a
slava <kishman201317@gmail.com>
2025-03-29 13:30:14 +0400

    add models.go and mod common code

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -26,6 +26,7 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 
 func (app *application) writeJSON(w http.ResponseWriter, status int, data envelope, headers http.Header) error {
 	js, err := json.Marshal(data)
+
 	if err != nil {
 		return err
 	}
