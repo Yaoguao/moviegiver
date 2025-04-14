8e39c92ac5b0dfbd8c30b7e3037f6273f121790b
Yaoguao <glenioJ@yandex.ru>
2025-04-14 15:24:21 +0400

    add pagination

--- internal/data/filteres.go
+++ internal/data/filteres.go
@@ -1,6 +1,9 @@
 package data
 
-import "moviegiver.kish.net/internal/validator"
+import (
+	"moviegiver.kish.net/internal/validator"
+	"strings"
+)
 
 type Filters struct {
 	Page         int
@@ -17,4 +20,29 @@ func ValidateFilters(v *validator.Validator, f Filters) {
 	v.Check(f.PageSize <= 100, "page_size", "must be a maximum of 100")
 
 	v.Check(validator.PermittedValue(f.Sort, f.SortSafelist...), "sort", "invalid sort value")
+}
+
+func (f Filters) sortColumn() string {
+	for _, safeValue := range f.SortSafelist {
+		if f.Sort == safeValue {
+			return strings.TrimPrefix(f.Sort, "-")
+		}
+	}
+
+	panic("unsafe sort parameter: " + f.Sort)
+}
+
+func (f Filters) sortDirection() string {
+	if strings.HasPrefix(f.Sort, "-") {
+		return "DESC"
+	}
+	return "ASC"
+}
+
+func (f Filters) limit() int {
+	return f.PageSize
+}
+
+func (f Filters) offset() int {
+	return (f.Page - 1) * f.PageSize
 }
